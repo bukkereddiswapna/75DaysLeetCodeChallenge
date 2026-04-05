7f0026391df889000e47e0f20b42c7b8d679cb4e
bukkereddiswapna <reddiswapnabukke005@gmail.com>
2026-04-05 22:52:15 +0530

    Time: 0 ms (100%), Space: 43.8 MB (84.6%) - LeetHub

---
+++ 0074-search-a-2d-matrix/0074-search-a-2d-matrix.java
@@ -0,0 +1,19 @@
+class Solution {
+    public boolean searchMatrix(int[][] matrix, int target) {
+     int r=matrix.length;
+     int c=matrix[0].length;
+     int s=0,e=r*c-1;
+     while(s<=e){
+        int mid=(s+e)/2;
+        int val=matrix[mid/c][mid%c];
+        if(val==target){
+            return true;
+        }else if(val>target){
+            e=mid-1;
+        }else{
+            s=mid+1;
+        }
+     }   
+     return false;
+    }
+}
